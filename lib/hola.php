06a442874d232542aedfce1aa22778864b8dc180
danielvl <dviclun1409@g.educaand.es>
2023-10-17 10:04:14 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,7 @@
 <?php
 	//Autor: Daniel <dviclun1409@g.educaand.es>
 	//Comentario
-	$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-	@print "Hola {$argv[1]}\n";
+	print "Introduce tu nombre";
+	$nombre = trim(fgets(STDIN));
+	@print "Hola {$nombre}\n";
 ?>
